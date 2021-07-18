2105016024d2c45c6d69452fcb89d43c753a5718
tumat001 <castromatthieu23@gmail.com>
2021-07-18 09:54:48 +0800

    Minor changes. Making this to make stable release.

--- Codes/PortalRelated/ActionsAndQueryRelated/Queries/IfAccountExistsQuery.vb
+++ Codes/PortalRelated/ActionsAndQueryRelated/Queries/IfAccountExistsQuery.vb
@@ -14,25 +14,28 @@ Public Class IfAccountExistsQuery
     ''' <returns>True if the student account with the given username exists, false otherwise</returns>
     ''' <exception cref="StringConstraintsViolatedException"></exception>
     Public Function IfStudentAccountExists(username As String) As Boolean
-        PortalQueriesAndActions.NewUsernameConstraint.Evaluate(username)
+        Try
+            PortalQueriesAndActions.NewUsernameConstraint.Evaluate(username)
 
-        Using connection As SqlConnection = New SqlConnection(PORTAL_DATABASE_CONNECTION_STRING)
-            connection.Open()
-            Dim command As SqlCommand = connection.CreateCommand()
+            Using connection As SqlConnection = New SqlConnection(PORTAL_DATABASE_CONNECTION_STRING)
+                connection.Open()
+                Dim command As SqlCommand = connection.CreateCommand()
 
-            command.CommandText = "SELECT  " + STUDENT_TABLE_USERNAME_COLUMN_NAME + " FROM [StudentAccountTable] WHERE " + STUDENT_TABLE_USERNAME_COLUMN_NAME + " = @UsernameValue"
-            command.Parameters.Add(New SqlParameter("StudentAccountTable", STUDENT_TABLE_NAME))
-            command.Parameters.Add(New SqlParameter("UsernameValue", username))
+                command.CommandText = "SELECT  " + STUDENT_TABLE_USERNAME_COLUMN_NAME + " FROM [StudentAccountTable] WHERE " + STUDENT_TABLE_USERNAME_COLUMN_NAME + " = @UsernameValue"
+                command.Parameters.Add(New SqlParameter("StudentAccountTable", STUDENT_TABLE_NAME))
+                command.Parameters.Add(New SqlParameter("UsernameValue", username))
 
-            Dim studentReader As SqlDataReader = command.ExecuteReader()
-            Dim studentFound As Boolean = studentReader.Read()
-            studentReader.Close()
+                Dim studentReader As SqlDataReader = command.ExecuteReader()
+                Dim studentFound As Boolean = studentReader.Read()
+                studentReader.Close()
 
-            Return studentFound
+                Return studentFound
 
+            End Using
+
+        Catch ex As SqlException
             Return False
-
-        End Using
+        End Try
     End Function
 
     ''' <summary>
@@ -42,24 +45,29 @@ Public Class IfAccountExistsQuery
     ''' <returns>True if the admin account with the given username exists, false otherwise</returns>
     ''' <exception cref="StringConstraintsViolatedException"></exception>
     Public Function IfAdminAccountExists(username As String) As Boolean
-        PortalQueriesAndActions.NewUsernameConstraint.Evaluate(username)
+        Try
+            PortalQueriesAndActions.NewUsernameConstraint.Evaluate(username)
 
-        Dim connection As SqlConnection = New SqlConnection(PORTAL_DATABASE_CONNECTION_STRING)
-        connection.Open()
-        Dim command As SqlCommand = connection.CreateCommand()
+            Dim connection As SqlConnection = New SqlConnection(PORTAL_DATABASE_CONNECTION_STRING)
+            connection.Open()
+            Dim command As SqlCommand = connection.CreateCommand()
 
-        command.CommandText = "SELECT * FROM [AdminAccountTable] WHERE " + ADMIN_TABLE_USERNAME_COLUMN_NAME + " = @UsernameValue"
-        command.Parameters.Add(New SqlParameter("AdminAccountTable", ADMIN_TABLE_NAME))
-        command.Parameters.Add(New SqlParameter("UsernameValue", username))
+            command.CommandText = "SELECT * FROM [AdminAccountTable] WHERE " + ADMIN_TABLE_USERNAME_COLUMN_NAME + " = @UsernameValue"
+            command.Parameters.Add(New SqlParameter("AdminAccountTable", ADMIN_TABLE_NAME))
+            command.Parameters.Add(New SqlParameter("UsernameValue", username))
 
-        Dim adminReader As SqlDataReader = command.ExecuteReader()
-        If adminReader.Read Then
-            connection.Close()
-            Return True
-        Else
-            connection.Close()
+            Dim adminReader As SqlDataReader = command.ExecuteReader()
+            If adminReader.Read Then
+                connection.Close()
+                Return True
+            Else
+                connection.Close()
+                Return False
+            End If
+
+        Catch ex As SqlException
             Return False
-        End If
+        End Try
     End Function
 
 End Class
